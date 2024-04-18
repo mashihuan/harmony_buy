e30bb35b23979d0377a00d73fdcc07d220396e85
mashihuan <1067615605@qq.com>
2024-04-18 17:06:02 +0800

    初版提交

---
+++ README.md
@@ -0,0 +1,19 @@
+## harmonyOS4.0重构之前的uni-app项目
+
+### 部分页面预览
+
+<img src="https://harmony.api.mashihuan.com/preview/home.png" height="400px">
+
+<img src="https://harmony.api.mashihuan.com/preview/category.png" height="400px">
+
+<img src="https://harmony.api.mashihuan.com/preview/goods_detail.png" height="400px">
+
+<img src="https://harmony.api.mashihuan.com/preview/menu.png" height="400px">
+
+<img src="https://harmony.api.mashihuan.com/preview/menu_detail.png" height="400px">
+
+<img src="https://harmony.api.mashihuan.com/preview/cart.png" height="400px">
+
+<img src="https://harmony.api.mashihuan.com/preview/my.png" height="400px">
+
+<img src="https://harmony.api.mashihuan.com/preview/order.png" height="400px">
---
+++ entry/src/main/ets/common/Constants.ts
@@ -0,0 +1,24 @@
+export class Constants {
+  // rdb store name
+  static readonly RDB_NAME: string = 'Buy.db'
+
+  // preferences store name
+  static readonly PREF_NAME: string = 'BuyStore'
+
+  // width 比例
+  static readonly width_100: string = '100%'
+  static readonly width_96: string = '96%'
+  static readonly width_94: string = '94%'
+
+  // space 间距
+  static readonly SPACE_2: number = 2;
+  static readonly SPACE_3: number = 2;
+  static readonly SPACE_4: number = 4;
+  static readonly SPACE_5: number = 4;
+  static readonly SPACE_6: number = 6;
+  static readonly SPACE_8: number = 8;
+  static readonly SPACE_10: number = 10;
+  static readonly SPACE_12: number = 12;
+  static readonly SPACE_14: number = 14;
+  static readonly SPACE_16: number = 16;
+}
